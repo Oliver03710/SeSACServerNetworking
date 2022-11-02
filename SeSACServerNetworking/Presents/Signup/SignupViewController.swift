ae41100965c483bbb9953e84d5243984a00768a1
Oliver03710 <jhee2619@gmail.com>
2022-11-03 01:27:12 +0900

    Networking Test

--- SeSACServerNetworking/Presents/Signup/SignupViewController.swift
+++ SeSACServerNetworking/Presents/Signup/SignupViewController.swift
@@ -18,7 +18,24 @@ final class SignupViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        SeSACManager.shared.request(Login.self, router: SeSACAPI.login(email: "testJack1@testJack.com", password: "12345678")) { data in
+            UserdefaultsHelper.standard.personalToken = data.token
+            print(data)
+        } failure: { error in
+            print(error)
+        }
         
+//        SeSACManager.shared.request(router: SeSACAPI.signup(userName: "Jacks0005", email: "Jacks0005@testJack.com", password: "12345678")) { data in
+//            print(data)
+//        } failure: { error in
+//            print(error)
+//        }
+        
+//        SeSACManager.shared.request(Profile.self, router: SeSACAPI.profile) { data in
+//            print(data)
+//        } failure: { error in
+//            print(error)
+//        }
     }
 
 }
