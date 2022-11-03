31020e9ea57a616add72c2bad991932d2a3293de
Oliver03710 <jhee2619@gmail.com>
2022-11-03 21:04:58 +0900

    Create Request API Method Using Single

--- SeSACServerNetworking/Utilities/Networking/SeSACManager.swift
+++ SeSACServerNetworking/Utilities/Networking/SeSACManager.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 import Alamofire
+import RxSwift
 
 final class SeSACManager {
 
@@ -23,17 +24,39 @@ final class SeSACManager {
     
     
     // MARK: - Helper Functions
+        
+    func request<T: Codable>(_ types: T.Type = T.self, router: SeSACAPI) -> Single<T> {
+        return Single<T>.create { single in
+            
+            AF.request(router).validate(statusCode: 200..<400).responseDecodable(of: types.self) { response in
+                
+                switch response.result {
+                case .success(let value):
+                    single(.success(value))
+                case .failure(let error):
+                    single(.failure(error))
+                }
+                
+            }
+            return Disposables.create()
+        }
+    }
     
-    func request<T: Codable>(_ types: T.Type = T.self, router: SeSACAPI, success: @escaping (T) -> Void, failure: @escaping (Error) -> Void) {
-        AF.request(router).validate(statusCode: 200..<400).responseDecodable(of: types.self) { response in
+    func request(router: SeSACAPI) -> Single<String> {
+        return Single<String>.create { single in
+            
+            AF.request(router).validate(statusCode: 200..<400).responseString() { response in
                 switch response.result {
                 case .success(let data):
-                    success(data)
-                case .failure(let err):
-                    failure(err)
+                    single(.success(data))
+                case .failure(let error):
+                    single(.failure(error))
                 }
             }
+            return Disposables.create()
         }
+    }
+
     
     func request(router: SeSACAPI, success: @escaping (String) -> Void, failure: @escaping (Error) -> Void) {
         AF.request(router).validate(statusCode: 200..<400).responseString() { response in
@@ -46,3 +69,5 @@ final class SeSACManager {
             }
         }
 }
+
+
