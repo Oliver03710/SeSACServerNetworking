638c07d0eef627bad2216a7f3c3a914405376434
Oliver03710 <jhee2619@gmail.com>
2022-11-04 01:50:06 +0900

    Apply Property Wrappers / Refactor Logics

--- SeSACServerNetworking/Scene/Signup/SignupViewController.swift
+++ SeSACServerNetworking/Scene/Signup/SignupViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import RxCocoa
 import RxSwift
 import SnapKit
+import Toast
 
 final class SignupViewController: BaseViewController {
 
@@ -18,30 +19,30 @@ final class SignupViewController: BaseViewController {
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.font = .boldSystemFont(ofSize: 32)
-        label.text = "회원가입"
+        label.text = UserDefaultsManager.loginTitle
         label.textAlignment = .center
         return label
     }()
     
     private let nameTextField: CustomTextField = {
-        let tf = CustomTextField(placeholder: "이름을 입력하세요.", fontSize: 15)
+        let tf = CustomTextField(placeholder: UserDefaultsManager.nameTextField, fontSize: 15)
         return tf
     }()
     
     private let emailTextField: CustomTextField = {
-        let tf = CustomTextField(placeholder: "이메일주소를 입력하세요.", fontSize: 15)
+        let tf = CustomTextField(placeholder: UserDefaultsManager.emailTextField, fontSize: 15)
         return tf
     }()
     
     private let passwordTextField: CustomTextField = {
-        let tf = CustomTextField(placeholder: "비밀번호를 입력하세요.", fontSize: 15)
+        let tf = CustomTextField(placeholder: UserDefaultsManager.passwordTextField, fontSize: 15)
         tf.isSecureTextEntry = true
         return tf
     }()
     
     private let signupButton: UIButton = {
         let btn = UIButton()
-        btn.setTitle("회원가입", for: .normal)
+        btn.setTitle(UserDefaultsManager.loginTitle, for: .normal)
         btn.titleLabel?.font = .boldSystemFont(ofSize: 20)
         btn.backgroundColor = .darkGray
         btn.clipsToBounds = true
@@ -70,38 +71,38 @@ final class SignupViewController: BaseViewController {
     override func setConstraints() {
         [nameTextField, titleLabel, emailTextField, passwordTextField, signupButton].forEach { view.addSubview($0) }
         
-        nameTextField.snp.makeConstraints { [unowned self] make in
-            make.centerY.equalTo(self.view.snp.centerY).multipliedBy(0.65)
-            make.centerX.equalTo(self.view.snp.centerX)
-            make.width.equalTo(self.view.snp.width).dividedBy(1.5)
+        nameTextField.snp.makeConstraints { make in
+            make.centerY.equalTo(view.snp.centerY).multipliedBy(0.65)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(view.snp.width).dividedBy(1.5)
             make.height.equalTo(44)
         }
         
-        titleLabel.snp.makeConstraints { [unowned self] make in
+        titleLabel.snp.makeConstraints { make in
             make.bottom.equalTo(nameTextField.snp.top).offset(-60)
-            make.centerX.equalTo(self.view.snp.centerX)
-            make.width.equalTo(self.view.snp.width).dividedBy(1.5)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(view.snp.width).dividedBy(1.5)
             make.height.equalTo(44)
         }
         
-        emailTextField.snp.makeConstraints { [unowned self] make in
+        emailTextField.snp.makeConstraints { make in
             make.top.equalTo(nameTextField.snp.bottom).offset(40)
-            make.centerX.equalTo(self.view.snp.centerX)
-            make.width.equalTo(self.view.snp.width).dividedBy(1.5)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(view.snp.width).dividedBy(1.5)
             make.height.equalTo(44)
         }
         
-        passwordTextField.snp.makeConstraints { [unowned self] make in
+        passwordTextField.snp.makeConstraints { make in
             make.top.equalTo(emailTextField.snp.bottom).offset(40)
-            make.centerX.equalTo(self.view.snp.centerX)
-            make.width.equalTo(self.view.snp.width).dividedBy(1.5)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(view.snp.width).dividedBy(1.5)
             make.height.equalTo(44)
         }
         
-        signupButton.snp.makeConstraints { [unowned self] make in
+        signupButton.snp.makeConstraints { make in
             make.top.equalTo(passwordTextField.snp.bottom).offset(40)
-            make.centerX.equalTo(self.view.snp.centerX)
-            make.width.equalTo(self.view.snp.width).dividedBy(1.5)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(view.snp.width).dividedBy(1.5)
             make.height.equalTo(52)
         }
     }
@@ -124,15 +125,15 @@ final class SignupViewController: BaseViewController {
             .flatMapLatest { (vc, _) in
                 return SeSACManager.shared.request(router: vc.viewModel.api)
                 }
-            .withUnretained(self)
-            .subscribe { (vcs, data) in
-                if data.uppercased() == "OK" {
-                    let vc = LoginViewController()
-                    vc.modalPresentationStyle = .overFullScreen
-                    vcs.present(vc, animated: true)
-                }
-            } onError: { error in
-                print(error)
+            .subscribe { data in
+                guard data.uppercased() == UserDefaultsManager.okay else { return }
+                let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+                let sceneDelegate = windowScene?.delegate as? SceneDelegate
+                let vc = LoginViewController()
+                sceneDelegate?.window?.rootViewController = vc
+                sceneDelegate?.window?.makeKeyAndVisible()
+            } onError: { [weak self] error in
+                self?.view.makeToast(error.localizedDescription)
             }
             .disposed(by: disposeBag)
     }
